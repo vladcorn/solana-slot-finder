61e1a923204d046d027ff15df805928fda0efad7
vladpetrovich <vlad.petrovich@ideasoft.io>
2024-10-16 22:22:53 +0300

    - fix deploy

--- src/utils/get-block.util.ts
+++ src/utils/get-block.util.ts
@@ -3,15 +3,15 @@ import {Connection} from '@solana/web3.js';
 const connection = new Connection("https://solana-mainnet.gateway.tatum.io", {
     commitment: 'confirmed',
     httpHeaders: {
-        'x-api-key': 't-670fb1ff9b6c1e2be72debc4-2ae19177bc1e4bbc9eca0140'
+        'x-api-key': import.meta.env.VITE_SOLANA_RPC_TOKEN,
     }
 });
 
 export async function getBlockTime(slot: number): Promise<number | null> {
     try {
         return await connection.getBlockTime(slot);
-    } catch (e) {
-        if (e.message === `Slot ${slot} was skipped, or missing in long-term storage`) {
+    } catch (e: unknown) {
+        if ((e as Error).message === `Slot ${slot} was skipped, or missing in long-term storage`) {
             console.log(`Slot ${slot} was skipped, or missing in long-term storage. Trying to get block time for slot ${slot - 5}`);
             return await connection.getBlockTime(slot - 5);
         }
